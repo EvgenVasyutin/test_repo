82d076e0712adb6a036bfd89e5b12bbe7ca66d36
DmitryTok <FoB09@ukr.net>
2024-04-14 13:42:17 +0200

    registration, categories

--- book_exchange/books/urls.py
+++ book_exchange/books/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeDoneView,
+    PasswordChangeView,
+    PasswordResetCompleteView,
+)
+from django.urls import path
+
+from books.views import index, register, logout_user, login_user, categories
+
 urlpatterns = [
-    path('', view=views.index, name='index'),
+    path('', view=index, name='index'),
+    path('category/', view=categories, name='category'),
+    path('register/', view=register, name='register'),
+    path('login/', view=login_user, name='login'),
+    path('logout/', view=logout_user, name='logout'),
 ]
--- book_exchange/books/views.py
+++ book_exchange/books/views.py
@@ -1,9 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
-from books.models import Book
+from books.models import Book, Category
+from django.contrib.auth import authenticate, login, logout
+from books.forms import SignUpForm
+from django.contrib import messages
 
 
 def index(request: HttpRequest) -> HttpResponse:
     all_books = Book.objects.all()
     context = {'books': all_books}
     return render(request, 'index.html', context)
+
+
+def categories(request: HttpRequest) -> HttpResponse:
+    all_categories = Category.objects.all()
+    context = {'all_categories': all_categories}
+    return render(request, 'categories.html', context)
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, ('Ви успішно зареєструвалися! Ласкаво просимо!')
+            )
+            return redirect('index')
+    context = {'form': form}
+
+    return render(request, 'register.html', context)
+
+
+def login_user(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Ви увійшли в систему!')
+            return redirect('index')
+        else:
+            messages.success(
+                request, ('Під час входу сталася помилка. Повторіть спробу..')
+            )
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    messages.success(request, ('Ви успішно вийшли з системи!'))
+    return redirect('index')
